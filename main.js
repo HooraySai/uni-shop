6e6f0990902b6b7e947cfccedc2bf5a8ba298406
wangsai <1079265107@qq.com>
2023-05-09 21:19:33 +0800

    home

--- main.js
+++ main.js
@@ -2,7 +2,32 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
 
+uni.$http = $http
+
+// 配置请求跟路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+uni.$showMsg = function(title='数据加载失败', duration=1500) {
+	uni.showToast({
+		title: title,
+		duration:duration,
+		icon: 'none'
+	})
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
